e63966fec50efbf8bed790ade59d8869803430c9
Js-Brecht <1935258+Js-Brecht@users.noreply.github.com>
2023-01-07 01:05:56 -0900

    refactor: simplify omit function

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,14 @@
-type ValuesOf<T extends any[]> = T[number];
+import omit from "lodash/omit";
+
 /**
  * @param {Object} obj Any object you want to exclude keys from
- * @param {string[]} omit The keys you want to exclude from the object
+ * @param {string[]} omitKeyPaths The keys you want to exclude from the object
  * @returns {Object} Returns the original object, less the keys instructed to omit
  * @private
  */
 export function omitKeys<
-	T,
-	K extends (keyof T)[] = (keyof T)[],
-	X extends string | number | symbol = ValuesOf<K>,
-	R = Omit<T, X>
->(obj: T, ...omit: K): R {
-	for (const key of omit) {
-		if (key in obj) {
-			delete obj[key];
-		}
-	}
-	return ((obj as unknown) as R);
+    T extends Record<keyof any, any>,
+    K extends Array<keyof T> | Array<keyof T | string>[],
+>(obj: T, ...omitKeyPaths: K): Omit<T, Extract<K, keyof T>> {
+    return omit(obj, ...omitKeyPaths) as any;
 }
